d6970b00f2ec412efe18d7eab102f7d6335ec0b7
etrytuyj <e5e6r87iuy@aca.co.kr>
2020-10-25 10:19:30 +0900

    2020-10-25

---
+++ Vuejs/vue01docs/aa.js
@@ -0,0 +1,69 @@
+//import axios from 
+;
+
+new Vue({
+    /* edtmc^2w */
+    el: "",
+    data: {
+
+    },
+    //template: ``,
+    methods: {
+
+    },
+    components: { }, 
+    computed: {
+
+    },
+    watch: {
+
+    },
+    mounted: function() {
+        console.log("mounted");
+    },
+    updated: function() {
+        console.log("updated");
+    },
+});
+
+Vue.component("aa", { 
+    /* pdtmc^2w */
+    props: [] ,
+    data: function() {
+        return { }
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: { },
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
+
+Vue.component("aa", { 
+    /* pdtmc^2w */
+    props: [] ,
+    data: function() {
+        return { }
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: { },
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
